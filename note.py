e40d1fdc1dd3e1d3129984c347e81fc7af2c9ebf
KajetanFrackowiak <kajtek.gdynia@gmail.com>
2023-12-02 09:21:22 +0100

    /

--- note.py
+++ note.py
@@ -1,47 +1,27 @@
-# import matplotlib.pyplot as plt
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import precision_score, f1_score
 
-# from sklearn import datasets
-# from sklearn import svm
+data = load_iris()
 
-# digit = datasets.load_digits()
+X = data.data
+y = data.target
 
-# print(digit.data)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=27)
 
-import numpy as np 
-import matplotlib.pyplot as plt 
+model = LogisticRegression()
 
-years = ["2016", "2017", "2018", "2019"]
-cats = np.array([50, 45, 37, 30])
-dogs = np.array([40, 39, 50, 55])
-hamsters = np.array([10, 16, 13, 15])
-  
-# create x-axis values depending on the number of years
-x_axis = np.arange(len(years))
+model.fit(X_train, y_train)
+ 
+y_pred = model.predict(X_test)
 
-# increase the figure size 
-plt.figure(figsize=(12, 8))
+y_proba = model.predict_proba(X_test)
+print(y_proba[0])
+print(y_pred[0])
 
-# plt.bar(x_axis-0.2, cats, width=0.4, label='Cats')
-# plt.bar(x_axis+0.2, dogs, width=0.4, label='Dogs')
-  
-films = ['Wonder Woman', 'Sonic', '1917', 'Star Wars', 'Onward']
-box_office = [16.7, 26.1, 37.0, 34.5, 10.6]
+precision = precision_score(y_test, y_pred, average='macro')
+print(precision)
 
-# plotting the chart horizontally
-plt.barh(films, box_office)
-  
-# plt.bar(years, cats, label='Cats')
-# plt.bar(years, dogs, bottom=cats, label='Dogs')
-# plt.bar(years, hamsters, bottom=cats+dogs, label='Hamsters')
-
-# set tick labels and their location
-plt.xticks(x_axis, years)
-
-plt.xlabel('Years', fontsize=14)
-plt.ylabel('Preference (%)', fontsize=14)
-plt.title('The results of cat/dog survey', fontsize=20)
-
-# add legend 
-plt.legend()
-
-plt.show()
+f1 = f1_score(y_test, y_pred, average='macro')
+print(f1)
