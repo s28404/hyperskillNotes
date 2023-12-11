975425c17b5ac8ba605ec16f9dddba72c3a4307e
Kajetan <kajtek.gdynia@gmail.com>
2023-12-11 17:32:48 +0100

    .

--- README.md
+++
@@ -1,1 +0,0 @@
-# Simple notes to Hyperskill's tasks.
--- note.py
+++ note.py
@@ -1,32 +1,44 @@
-# from sklearn.datasets import load_iris
-# from sklearn.model_selection import train_test_split
-# from sklearn.ensemble import RandomForestClassifier
+# import nltk
+# from nltk.corpus import brown
+# from gensim.models import Word2Vec
 
-# X, y = load_iris(return_X_y=True)
-# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=27)
+# nltk.download('brown')
 
-# model = RandomForestClassifier(n_estimators=25,
-#                                max_features=3,
-#                                oob_score=True,
-#                                random_state=42)
+# # .sents() creates the corpus as a list of sentences
+# data = brown.sents()
 
-# model.fit(X_train, y_train)
+# w2v_model = Word2Vec(min_count=5,
+#                      window=5,
+#                      sg=0,
+#                      sample=6e-5,
+#                      negative=20)
 
-# y_pred = model.predict(X_test)
+# w2v_model.build_vocab(data)
+# w2v_model.train(data, total_examples=w2v_model.corpus_count, epochs=15)
 
-# y_proba = model.predict_proba(X_test)
-# print(y_proba[7])
-# print(y_pred[7])
+# # Access the trained embeddings
+# word_embeddings = w2v_model.wv
 
-# print(model.oob_score_)
-# print(model.oob_decision_function_[6])
-# print(model.oob_decision_function_[7])
+# # Use most_similar through the wv attribute
+# similar_words = word_embeddings.most_similar('university', topn=5)
+
+# Print the results
+# print("Similar words to 'university':")
+# for word, score in similar_words:
+    # print(f"{word}: {score}")
 
 
-# print(len(model.estimators_))
+# similarity_score = word_embeddings.similarity('jazz', 'music')
+# print(similarity_score)
+# d_match = w2v_model.doesnt_match(['pine', 'fir', 'coconut'])
+# print(d_match)
 
-# # make a prediction using 0th tree
-# tree = model.estimators_[0]
-# print(tree.predict(X_test[:10]))
+# import gensim.downloader as api
 
-import os
+# glove_model = api.load('glove-twitter-25')
+
+# most_similar = glove_model.most_similar(positive=['woman', 'king'], negative=['man'], topn=3)
+# print(most_similar)
+
+import fasttext
+model = fasttext.train_unsupervised("brown_text.txt", epoch=3, minn=2, maxn=4, dim=128)
