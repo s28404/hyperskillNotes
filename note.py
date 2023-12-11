eb5e040112aa399bf6055f3573d21ffc01ce47a0
Kajetan <kajtek.gdynia@gmail.com>
2023-12-11 19:27:34 +0100

    /

--- note.py
+++ note.py
@@ -40,5 +40,60 @@
 # most_similar = glove_model.most_similar(positive=['woman', 'king'], negative=['man'], topn=3)
 # print(most_similar)
 
-import fasttext
-model = fasttext.train_unsupervised("brown_text.txt", epoch=3, minn=2, maxn=4, dim=128)
+# import fasttext
+# model = fasttext.train_unsupervised("brown_text.txt", epoch=3, minn=2, maxn=4, dim=128)
+
+# import numpy as np
+# from nltk.tokenize import word_tokenize
+# from nltk import FreqDist
+
+
+# def one_hot_encoding(sentence):
+#     tokens = word_tokenize(sentence)
+
+#     freq_dist = FreqDist(tokens)
+
+#     unique_words = list(freq_dist.keys())
+
+#     one_hot_matrix = np.eye(len(unique_words))
+
+#     word_index = {word: i for i, word in enumerate(unique_words)}
+
+#     sentence_matrix = np.zeros((len(tokens), len(unique_words)))
+
+#     for i, token in enumerate(tokens):
+#         sentence_matrix[i, word_index[token]] = 1
+
+#     return sentence_matrix
+
+
+# path = 'data/dataset/input.txt'
+
+# with open(path, 'r') as file:
+#     sentence = file.read()
+
+# output_matrix = one_hot_encoding(sentence)
+
+# print(output_matrix)
+
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+vectorizer = TfidfVectorizer()
+
+dataset = ["So no one told you life was gonna be this way",
+           "Your job's a joke, you're broke",
+           "Your love life's DOA",
+           "It's like you're always stuck in second gear",
+           "When it hasn't been your day, your week, your month",
+           "Or even your year, but",
+           "I'll be there for you"]
+
+
+vectorizer = TfidfVectorizer(input='content', use_idf=True, lowercase=True,
+                             analyzer='word', ngram_range=(1, 1), stop_words=None, vocabulary=None, min_df=0.01, max_df=0.60)
+
+tfidf_matrix = vectorizer.fit_transform(dataset)
+
+print(f"Matrix dimension: {tfidf_matrix.shape}")
+# print(tfidf_matrix)
+print(tfidf_matrix[6])
